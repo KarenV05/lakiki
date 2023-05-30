dea19f03a030b1803e06ccd2e1a3d12975e9caf6
karen <karenskidrauhl22@gmail.com>
2023-05-30 14:00:22 -0700

    screen change

--- app/src/main/java/com/example/lakiki/MainActivity.java
+++ app/src/main/java/com/example/lakiki/MainActivity.java
@@ -51,17 +51,17 @@ public class MainActivity extends AppCompatActivity {
 
                 String story = "";
                 story += etMonth.getText() + "30th is at its end, its time to dress up for the annual trend.\n";
-                story += "I can feel the " + etAdj1.getText() + " air that signals october’s begining, hearing the never-ending" + etAdj2.getText() + " phone ringing.\n";
-                story += "Dressing up as a " + etCostume.getText() + "to go trick or treating, I'll be pulling up to every house like a" + etAdj3.getText() + " meeting.\n";
-                story += "Counting these racks, that's " + etNumber.getText() + "stacks.\n";
-                story += "Knocking on the door, " + etCelebrity.getText() + "opens up giving me more ‘cause that's what im here for.\n";
+                story += "I can feel the " + etAdj1.getText() + " air that signals october’s begining, hearing the never-ending " + etAdj2.getText() + " phone ringing.\n";
+                story += "Dressing up as a " + etCostume.getText() + " to go trick or treating, I'll be pulling up to every house like a " + etAdj3.getText() + " meeting.\n";
+                story += "Counting these racks, that's " + etNumber.getText() + " stacks.\n";
+                story += "Knocking on the door, " + etCelebrity.getText() + " opens up giving me more ‘cause that's what im here for.\n";
                 story += "Strutting " + etLocation.getText() + " like its a cat walk ‘cause that's what I know.\n";
                 story += "Serving these looks, the competition shook; its definitely one for the books.\n";
-                story += "Feasting on the treats, its true they gave me something " + etGoodorbad.getText() + "to eat.\n";
+                story += "Feasting on the treats, its true they gave me something " + etGoodorbad.getText() + " to eat.\n";
                 story += "Arriving home like a bird just when dinner is served.\n";
                 story += etSubstance.getText() + " filled tub, “oh you know I’m bout to grub” no crumbs.\n";
                 story += "I ate, clean plate.\n";
-                story += "Heading to beddy with my teddy, catching these hands if anyone ties to get me";
+                story += "Heading to beddy with my teddy, catching these hands if anyone tries to get me";
 
                 intentDisplay.putExtra("story",story);
 
